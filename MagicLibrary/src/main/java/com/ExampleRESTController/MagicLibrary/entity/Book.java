35459b04d12656ae2d10c23e63464c89d4529b79
Seweryn <sewek151@gmail.com>
2022-03-26 13:45:37 +0100

    Lombok introduction

--- MagicLibrary/src/main/java/com/ExampleRESTController/MagicLibrary/entity/Book.java
+++ MagicLibrary/src/main/java/com/ExampleRESTController/MagicLibrary/entity/Book.java
@@ -1,6 +1,11 @@
 package com.ExampleRESTController.MagicLibrary.entity;
 
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -8,6 +13,10 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 @Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class Book {
     @Id
     @GeneratedValue
@@ -18,64 +27,10 @@ public class Book {
     private boolean isAvailable;
 
 
-    public Book() {
-    }
-
     public Book(String title, String author, LocalDate printDate, boolean isAvailable) {
         this.title = title;
         this.author = author;
         this.printDate = printDate;
         this.isAvailable = isAvailable;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public LocalDate getPrintDate() {
-        return printDate;
-    }
-
-    public void setPrintDate(LocalDate printDate) {
-        this.printDate = printDate;
-    }
-
-    public boolean isAvailable() {
-        return isAvailable;
-    }
-
-    public void setAvailable(boolean available) {
-        isAvailable = available;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", author='" + author + '\'' +
-                ", printDate=" + printDate +
-                ", isAvailable=" + isAvailable +
-                '}';
     }
 }
